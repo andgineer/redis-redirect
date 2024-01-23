6f00435e8d7a9ab36078c2f7b58f29db43f5b6ca
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-01-23 06:24:58 +0100

    unpinned dependencies in Pysho package update deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as f:
+with open("requirements.in") as f:
     requirements = f.read().splitlines()
 
 from src.redis_redirect import version
